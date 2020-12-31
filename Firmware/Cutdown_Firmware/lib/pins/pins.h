eca5631cf7ae228a71f69a2508b4c70f23d7a68d
John R. Leeman <john@leemangeophysical.com>
2020-12-31 12:55:47 -0600

    Add cutdown firmware

---
+++ Firmware/Cutdown_Firmware/lib/pins/pins.h
@@ -0,0 +1,13 @@
+#ifndef PINS_h
+#define PINS_H
+
+// Hardware Pins
+const uint8_t PIN_SERIAL_TX = 1;
+const uint8_t PIN_SERIAL_RX = 0;
+const uint8_t PIN_XBEE_SERIAL_TX = 3;
+const uint8_t PIN_XBEE_SERIAL_RX = 2;
+const uint8_t PIN_LED_ERROR = 6;
+const uint8_t PIN_EXT_CUTDOWN = 8;
+const uint8_t PIN_HOTWIRE = 9;
+
+#endif
