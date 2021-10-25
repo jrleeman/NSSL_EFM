1ab5474e6efa2fbf5145e55d0ac3eb76d719ada4
John R. Leeman <john@leemangeophysical.com>
2021-10-25 08:18:03 -0500

    Make SD card write interval sensible (about 5 minutes)

--- Firmware/Control_SD_Firmware/src/main.cpp
+++ Firmware/Control_SD_Firmware/src/main.cpp
@@ -16,7 +16,7 @@
 
 
 char log_file_name[] = "EFM00.TXT";
-CircularBuffer<char, 700> CharBuffer;
+CircularBuffer<char, 500> CharBuffer;
 File dataFile;
 SdFat sd;
 uint32_t last_write = 0;
@@ -63,50 +63,21 @@ void setup()
   delay(1000);
   digitalWrite(PIN_LED_ACTIVITY, LOW);
   digitalWrite(PIN_LED_ERROR, LOW);
-  Serial.begin(38400);
-  // We're not going to use CS since this is the only device, set it high
-  //digitalWrite(PIN_SD_CS, HIGH);
+  Serial.begin(57600);
+
   if (!sd.begin(PIN_SD_CS, SPI_SPEED))
   {
     error();
   }
+
   setLogFileName();
-  //dataFile = sd.open(log_file_name, FILE_WRITE);
 }
 
 void loop()
 {
   static uint8_t file_is_open = 1;
-  static uint32_t write_cycles = 6000000;
-  //dataFile = sd.open(log_file_name, FILE_WRITE);
-  /*
-  if ((millis() - last_write) > 10000)
-  {
-    digitalWrite(PIN_LED_ACTIVITY, HIGH);
-    delay(20); 
-    //dataFile.flush();
-    dataFile.close();
-    dataFile = sd.open(log_file_name, FILE_WRITE);
-    digitalWrite(PIN_LED_ACTIVITY, LOW);
-    last_write = millis();
-  }
-  */
-  /*
-  if(Serial.available())
-  {   
-    digitalWrite(PIN_LED_ACTIVITY, HIGH);
-    dataFile = sd.open(log_file_name, FILE_WRITE);
-    uint16_t buffer_size = Serial.available();
-    char buff[500];
-    for (uint16_t i=0; i<buffer_size; i++)
-    {
-      buff[i] = Serial.read();
-    } 
-    dataFile.write(buff, buffer_size);  
-    dataFile.close();
-    digitalWrite(PIN_LED_ACTIVITY, LOW);
-  }
-  */
+  static uint32_t write_cycles = 100000;
+
  while(Serial.available())
   {   
     //digitalWrite(PIN_LED_ERROR, HIGH);
@@ -114,7 +85,7 @@ void loop()
     //digitalWrite(PIN_LED_ERROR, LOW);
   }
 
-  if (write_cycles > 600000)
+  if (write_cycles > 335000)
   {
     dataFile.close();
     setLogFileName();
@@ -123,18 +94,18 @@ void loop()
   }
 
   if (CharBuffer.isFull())
-  {digitalWrite(PIN_LED_ERROR, HIGH);}
-
-  if (CharBuffer.size() > 10)
   {
-    //digitalWrite(PIN_LED_ACTIVITY, HIGH);
-    //while (!CharBuffer.isEmpty())
+    digitalWrite(PIN_LED_ERROR, HIGH);
+  }
+
+  if (CharBuffer.size() >= 100)
+  {
+    digitalWrite(PIN_LED_ACTIVITY, HIGH);
     for (int j=0; j<CharBuffer.size(); j++)
     {
       dataFile.write(CharBuffer.shift());  
       write_cycles += 1;
     }
-    //digitalWrite(PIN_LED_ACTIVITY, LOW);
-    
+    digitalWrite(PIN_LED_ACTIVITY, LOW);
   }
 }
